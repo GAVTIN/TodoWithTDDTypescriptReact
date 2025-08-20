4039e81ac69de7b741489f99d91297546f6a7388
gavtin <gauravkumarsinhasinha@gmail.com>
2025-08-20 19:17:53 +0530

    [Commit Changes After Initial Build Of Todo Before Unit Test Setup]

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,50 @@
+import { useState } from 'react';
+import './index.css';
+type Priority = 'p1' | 'p2' | 'p3';
+
+type Task = {
+  id: number;
+  title: string;
+  isCompleted: boolean;
+  priority?: Priority;
+};
+
 function App() {
-  return <div>Test</div>;
+  const [tasks, setTasks] = useState<Task[]>([
+    {
+      id: 123,
+      title: 'React Todo',
+      isCompleted: false,
+    },
+  ]);
+  const [task, setTask] = useState<string>('');
+  const handleAddTask = () => {
+    setTasks((prev) => [
+      ...prev,
+      { id: Date.now(), title: task, isCompleted: false },
+    ]);
+    setTask('');
+  };
+  return (
+    <div>
+      <h1>Tasks</h1>
+      <input
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        type="text"
+        placeholder="Enter Task"
+        className="button-style"
+      />
+      <button className="button-style" onClick={handleAddTask}>
+        Add Task
+      </button>
+      <ul>
+        {tasks?.map((item) => {
+          return <li key={item.id}>{item.title}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
